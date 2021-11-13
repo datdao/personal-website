b6f19cf7eef90a5661bf985a6fa882396a6ef349
datdao <datdao.sde@gmail.com>
2021-11-13 23:18:55 +0700

    Change setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://datdao.me", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          siteName: "My personal website", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FFFFFF", // Used in manifest.json
-          themeColor: "#000000", // Used in manifest.json
+          backgroundColor: "#000000", // Used in manifest.json
+          themeColor: "#FFFFFF", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
